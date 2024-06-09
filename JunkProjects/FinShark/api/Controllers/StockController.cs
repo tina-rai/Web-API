b09c5b8bc83f7854953a8b21ed7e8372b7dc8b11
Tina Rai <161432879+tina-rai@users.noreply.github.com>
2024-06-09 21:29:42 +0545

    Create StockController.cs

---
+++ JunkProjects/FinShark/api/Controllers/StockController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[Controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock.ToStockDto());
+        }
+
+
+    }
+}
+
