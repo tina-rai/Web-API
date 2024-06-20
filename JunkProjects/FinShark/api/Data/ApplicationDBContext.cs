0a8b6d86837120521c650f6ce058c12aa395e40d
Tina Rai <161432879+tina-rai@users.noreply.github.com>
2024-06-20 19:47:18 +0545

    Update ApplicationDBContext.cs

--- JunkProjects/FinShark/api/Data/ApplicationDBContext.cs
+++ JunkProjects/FinShark/api/Data/ApplicationDBContext.cs
@@ -16,7 +16,7 @@ namespace api.Data
 
         }
 
-        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Stock> Stock{ get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
 }
