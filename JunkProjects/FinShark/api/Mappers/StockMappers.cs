7ee78555e3ecb53eac053c5b593319daa784f2d7
Tina Rai <161432879+tina-rai@users.noreply.github.com>
2024-06-20 19:55:09 +0545

    Update StockMappers.cs

--- JunkProjects/FinShark/api/Mappers/StockMappers.cs
+++ JunkProjects/FinShark/api/Mappers/StockMappers.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext//class, a part of the EFcore, works with database 
+                                                 //inherit from dbcontext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
